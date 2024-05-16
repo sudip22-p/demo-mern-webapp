33a6e3d86e0b2bfe69f1c98d677243e58446021d
sudip <paudelsudip134@gmail.com>
2024-05-16 09:49:14 +0545

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import Service from './components/Service';
+import Login from './components/Login';
+import Register from './components/Register';
 
 // import { BrowserRouter as Router, Route, Link, Routes }
 //     from "react-router-dom";
@@ -16,6 +21,11 @@ function App() {
           <Routes>
             {/* //may be another component here */}
             <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/service' element={<Service/>}/>
+            <Route path='/contact' element={<Contact/>}/>
           </Routes>
           {/* //footer always there */}
           <Footer/>
