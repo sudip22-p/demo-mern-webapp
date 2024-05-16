46948a01329f2fcff5beaff55e13c50cf59cdaef
sudip <paudelsudip134@gmail.com>
2024-05-16 10:05:16 +0545

    navbar menus route/link upd

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,10 +15,18 @@ function Navbar() {
                 <div className="logo font-bold text-red-600 py-2 cursor-pointer">Demo WebApp</div>
                 </Link>
                 <ul className='flex justify-between py-2'>
-                    <li className='mx-7 hover:text-blue-500 cursor-pointer'>Home</li>
-                    <li className='mx-7 hover:text-blue-500 cursor-pointer'>About Us</li>
-                    <li className='mx-7 hover:text-blue-500 cursor-pointer'>Services</li>
-                    <li className='mx-7 hover:text-blue-500 cursor-pointer'>Contact Us</li>
+                    <Link to={"/"}>
+                        <li className='mx-7 hover:text-blue-500 cursor-pointer'>Home</li>
+                    </Link>
+                    <Link to={"/about"}>
+                        <li className='mx-7 hover:text-blue-500 cursor-pointer'>About Us</li>
+                    </Link>
+                    <Link to={"/service"}>
+                        <li className='mx-7 hover:text-blue-500 cursor-pointer'>Services</li>
+                    </Link>
+                    <Link to={"/contact"}>
+                        <li className='mx-7 hover:text-blue-500 cursor-pointer'>Contact Us</li>
+                    </Link>
                 </ul>
                 <Link to={"/login"}>
                     <button className="login font-bold bg-white py-2 px-5 rounded text-blue-500 hover:bg-blue-500 hover:text-white border-2 border-black hover:border-blue-500 cursor-pointer">LOGIN</button>
