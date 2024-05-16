6cd5580f06cea316978f8eaec4a53fc4bc8c93ad
sudip <paudelsudip134@gmail.com>
2024-05-16 09:06:11 +0545

    components updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>
-      Hello from Home
-    </div>
-  )
+    return (
+        <main className="flex flex-col justify-center items-center h-screen bg-blue-500">
+            <h1 className='text-white font-bold'>Hey User!!</h1>
+            <p className="text-white font-semibold">Welcome to My Website</p>
+            <p className="text-white font-normal mt-3 mb-1">Login to continue,</p>
+            <div>
+                <button className="login font-bold bg-white py-2 px-5 rounded text-blue-500 hover:bg-blue-500 hover:text-white border-2 border-black hover:border-blue-500 cursor-pointer">LOGIN</button>
+            </div>
+        </main>
+    )
 }
 
 export default Home
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+// useState
 function Navbar() {
-  return (
-    <div>
-      Hello from Navy
-    </div>
-  )
+    //for showing and hiding the navbar--responsiveness
+    const [showMenu, setShowMenu] = useState(false)
+    //a function to reverse the showMenus value
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)//sets true if false and vice versa
+    }
+    return (
+        <header>
+            <nav className='bg-slate-200 text-black-600 flex justify-between px-7 py-5'>
+                <div className="logo font-bold text-red-600 py-2 cursor-pointer">Demo WebApp</div>
+                <ul className='flex justify-between py-2'>
+                    <li className='mx-7 hover:text-blue-500'><a href="#">Home</a></li>
+                    <li className='mx-7 hover:text-blue-500'><a href="#">About Us</a></li>
+                    <li className='mx-7 hover:text-blue-500'><a href="#">Services</a></li>
+                    <li className='mx-7 hover:text-blue-500'><a href="#">Contact Us</a></li>
+                </ul>
+                <div>
+                    <button className="login font-bold bg-white py-2 px-5 rounded text-blue-500 hover:bg-blue-500 hover:text-white border-2 border-black hover:border-blue-500 cursor-pointer">LOGIN</button>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
