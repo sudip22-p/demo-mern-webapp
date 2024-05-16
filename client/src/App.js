6fb37c9283766f74748c583bbb12cbd80159d5c7
sudip <paudelsudip134@gmail.com>
2024-05-16 08:23:23 +0545

    use of react-router for Nav/Home/Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 // import { BrowserRouter as Router, Route, Link, Routes }
 //     from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline bg-blue-500 text-white">
-      Hello world!
-    </h1>
+        <Router>
+          {/* //always there */}
+          <Navbar/>
+          <Routes>
+            {/* //may be another component here */}
+            <Route path='/' element={<Home/>}/>
+          </Routes>
+          {/* //footer always there */}
+          <Footer/>
+        </Router>
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Hey footer here
+    </div>
+  )
+}
+
+export default Footer
