bc4b6d43500569173da55e55e3b44489d630bcf6
sudip <paudelsudip134@gmail.com>
2024-05-16 11:43:23 +0545

    monor typos & style changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,14 +8,14 @@ function Login() {
       <form action='POST' method='/login' className="w-full max-w-md">
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700">Email:</label>
-          <input type="email" id="email" name="email" className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full" />
+          <input type="email" id="email" name="email" className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full" required/>
         </div>
 
         <div className="mb-6">
           <label htmlFor="password" className="block text-gray-700">Password:</label>
-          <input type="password" id="password" name="password" className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full" />
+          <input type="password" id="password" name="password" className="mt-1 px-4 py-2 border border-gray-300 rounded-md w-full" required/>
         </div>
-        <input type="submit" className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors cursor-pointer" value={"submit"}/>
+        <input type="submit" className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors cursor-pointer" value={"Login"}/>
       </form>
 
       <p className="mt-4">Don't have an account? <Link to={"/register"}><span className="text-blue-500 hover:underline">Register</span></Link></p>
