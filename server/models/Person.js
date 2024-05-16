64e3e141a42c4f7d91f98ca7c7cfcaf14c14643c
sudip <paudelsudip134@gmail.com>
2024-05-16 13:02:27 +0545

    person schema creation as Person model

---
+++ server/models/Person.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const personSchema = new mongoose.Schema({
+  name: String, // String is shorthand for {type: String}
+  email: String,
+  password: String,
+});
+//conversion of schema to model
+export const Person = mongoose.model('Person', personSchema);
