1edd587a10b82b6a1e67423b6491e4c7cfce943d
sudip <paudelsudip134@gmail.com>
2024-05-16 08:06:59 +0545

    importing the routes ,link and other fron react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link, Routes }
+//     from "react-router-dom";
 function App() {
   return (
     <div className="App">
